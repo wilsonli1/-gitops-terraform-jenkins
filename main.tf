17f4c5f2a7975de13f2e20edf2921fc6622f06d3
alexandarp <alexandar.podobnik@gmail.com>
2019-04-22 11:05:21 -0400

    add ec2 instance and security group

--- main.tf
+++ main.tf
@@ -11,3 +11,36 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+# Create EC2 instance
+resource "aws_instance" "default" {
+  ami                    = "${var.ami}"
+  count                  = "${var.count}"
+  key_name               = "${var.key_name}"
+  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+  source_dest_check      = false
+  instance_type          = "${var.instance_type}"
+
+  tags {
+    Name = "terraform-default"
+  }
+}
+
+# Create Security Group for EC2
+resource "aws_security_group" "default" {
+  name = "terraform-default-sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,20 @@
+variable "count" {
+  default = 1
+}
 
+variable "key_name" {
+  description = "Private key name to use with instance"
+  default     = "terraform"
+}
+
+variable "instance_type" {
+  description = "AWS instance type"
+  default     = "t3.small"
+}
+
+variable "ami" {
+  description = "Base AMI to launch the instances"
+
+  # Bitnami NGINX AMI
+  default = "ami-021acbdb89706aa89"
+}
