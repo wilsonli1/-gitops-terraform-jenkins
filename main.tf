a52ca3d0733c6fd03e7138e415726a82bbd68198
wilsonli1 <34107257+wilsonli1@users.noreply.github.com>
2019-12-06 16:45:15 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,12 +14,12 @@ provider "aws" {
 
 # Create EC2 instance
 resource "aws_instance" "default" {
-  ami                    = "${var.ami}"
-  count                  = "${var.count}"
-  key_name               = "${var.key_name}"
-  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+  ami                    = var.ami
+  count                  = "var.count
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.default.id]
   source_dest_check      = false
-  instance_type          = "${var.instance_type}"
+  instance_type          = var.instance_type
 
   tags {
     Name = "terraform-default"
