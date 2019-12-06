2d067f3acccc16bdba0abaa47fe65d950a588126
wilsonli1 <34107257+wilsonli1@users.noreply.github.com>
2019-12-06 16:47:47 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 # Create EC2 instance
 resource "aws_instance" "default" {
   ami                    = var.ami
-  count                  = "var.count
+  count                  = var.count
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.default.id]
   source_dest_check      = false
