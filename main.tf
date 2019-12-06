5ed9c73d881399c8b6294b07e2ea210ddddb5bee
wilsonli1 <34107257+wilsonli1@users.noreply.github.com>
2019-12-06 18:27:36 -0500

    adding 8080

--- main.tf
+++ main.tf
@@ -43,5 +43,11 @@ resource "aws_security_group" "default" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
