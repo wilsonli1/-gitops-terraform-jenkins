7d61f78fb71d89300135ed5e62c3187fbb0b4b26
wilsonli1 <34107257+wilsonli1@users.noreply.github.com>
2019-12-06 20:35:54 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_instance"  "test2"{
   ami                    = var.ami
   count                  = var.counter
   key_name               = var.key_name
-  vpc_security_group_ids = "terraform-default-sg2"
+  vpc_security_group_ids = [aws_security_group.sg-test2.id]
   source_dest_check      = false
   instance_type          = var.instance_type
 
